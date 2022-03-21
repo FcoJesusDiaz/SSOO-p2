61b131de63c6ce6176261f463ce4c3f462b6ed0a
FcoJesusDiaz <FcoJesus.Diaz@alu.uclm.es>
2022-03-21 21:16:20 +0100

    Creation of classes and structures

---
+++ lab2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <thread>
+#include <stdlib.h>
+#include <queue>
+#include <string>
+
+class Searcher{
+    private:
+        int id;
+        int begin;
+        int end;
+        std::queue<result> results;
+    
+    public:
+        Searcher(int id, int begin, int end){
+            this->id = id;
+            this->begin = begin;
+            this->end = end;
+        }
+        
+        void operator()(){
+
+        }
+};struct result{
+    std::string previous;
+    std::string next;
+};
+
+int main(int argc, char **argv){
+    return 0;
+}
