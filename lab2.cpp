d65852238bee2ed3c14e78a490f83b080c15b0fa
migueldelasheras <72572371+migueldelasheras@users.noreply.github.com>
2022-03-28 20:49:26 +0200

    Add files via upload

--- lab2.cpp
+++ lab2.cpp
@@ -3,50 +3,57 @@
 #include <stdlib.h>
 #include <queue>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <functional>
+
+#define N_HILOS 5
+
+struct Result{
+    std::string previous;
+    std::string next;
+    int line;
+};
 
 class Searcher{
     private:
         int id;
         int begin;
         int end;
-        std::queue<result> results;
+        //std::fstream fd;
+        std::queue<Result> results;
     
     public:
-        Searcher(int id, int begin, int end){
-            this->id = id;
-            this->begin = begin;
-            this->end = end;
-        }
+        Searcher(int id, int begin, int end): id(id), begin(begin), end(end){}
         
         void operator()(){
 
+            std::cout <<"HILO:" << id <<"Voy a buscar desde la linea"<< begin << "hasta la linea" << end <<"\n" << std::endl;
+
         }
 };
 
-struct result{
-    std::string previous;
-    std::string next;
-};
-
 int main(int argc, char **argv){
-    std::ifstream file(FILE_NAME);
-    if(!file.is_open()){
-        std::cout << "Error opening file" << std::endl;
-        exit(EXIT_FAILURE);
-    }
+
+    std::vector<std::thread> v_hilos;
+    std::vector<Searcher> v_objetos;
+    int l_begin;
+    int l_end;
     
-    int n_lines;
-    int subdivision;
-    std::string aux;
+    for (int i = 0; i < N_HILOS; i++)
+    {
+        l_begin=0;
+        l_end=i+90;
+        Searcher s{i+1,l_begin,l_end};
+        v_hilos.push_back(std::thread(s));
+        
+    }
 
+    std::for_each(v_hilos.begin(),v_hilos.end(),std::mem_fn(&std::thread::join));
 
-    while (std::getline(file, aux))
-        n_lines++;
+    
 
-    subdivision = n_lines/THREADS;
-
-    std::vector<int> thread_read_beginning;
-    for(int i = 0; i < THREADS; i++)
-        thread_read_beginning.push_back(i*subdivision);
     return 0;
 }
