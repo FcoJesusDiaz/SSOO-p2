e79ccd63fccd08a1892d9c5bc6e754eafa2c2b72
migueldelasheras <72572371+migueldelasheras@users.noreply.github.com>
2022-04-08 18:06:14 +0200

    actualizacion

--- SSOOIIGLE.cpp
+++ SSOOIIGLE.cpp
@@ -5,7 +5,6 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
-#include <condition_variable>
 
 #include "../include/Searcher.h"
 #include "../include/colors.h"
@@ -13,45 +12,70 @@
 int CountLines(std::string filename);
 void checkArguments(int argc, char **argv);
 bool is_integer(char *str);
+void assignTurns(int num_threads);
+void printResults(void);
 
+//array to hold the start byte of each line
 std::vector<int> numLines;
-std::mutex semaphore;
-std::mutex semTurn;
-std::condition_variable cv;
+//array used to store all final results
+std::vector<Result> Totalsearches;
+//semaphore used to restrict access to the flag variable
+std::mutex m;
+//vector used to restrict access to the vector
+std::mutex vectorLock;
+//variable to assign each thread a turn
+int flag;
 
 int main(int argc, char **argv){
 
     checkArguments(argc,argv);
 
-    int num_lines=CountLines(argv[1]);
-    std::cout << "The file has "<< num_lines <<" lines"<< std::endl;
-
-    //declaramos el numero de hilos que usaremos en el programa
     int num_threads=atoi(argv[3]);
 
-    //vectores en los que guardaremos los hilos creados y los objetos searcher
+    int num_lines=CountLines(argv[1]);
+
+    /* if the number of threads requested is greater than the number of lines of the indicated file; 
+    program ends */
+    if(num_lines<num_threads) {
+        std::cerr << RED<< "The number of threads is higher than the number of lines"<< RESET<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "The file has "<< num_lines <<" lines"<< std::endl;
+
+    //vectors in which we will store the created threads and the searcher instances
     std::vector<std::thread> v_hilos;
     std::vector<Searcher> v_objetos;
 
-    //Dividimos las lineas entre el numero de hilos creados
+    //lines of each thread
     int task_size = num_lines/num_threads;
 
     for (int i = 0; i < num_threads; i++)
     {
-        //variables en las que pondremos la linea de inicio y de final de cada hilo
+        /* variables indicating the start and end line of each thread.  if it is the last thread; it is 
+        assigned until the last line */
         int l_begin= i*task_size;
-        int l_end= (l_begin+task_size)-1;
+        int l_end;
+        if(i!=num_threads-1) l_end= (l_begin+task_size)-1;
+        else l_end=num_lines-1;
 
         Searcher s{i+1,l_begin,l_end,argv[1],argv[2]};
         v_hilos.push_back(std::thread(s));
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
+    //round robin so that each thread stores the search results in the global array (Totalsearches)
+    assignTurns(num_threads);
+
+    //wait until all threads finish
     std::for_each(v_hilos.begin(),v_hilos.end(),std::mem_fn(&std::thread::join));
+
+    printResults();
     
     return 0;
 }
-
+/* method to count the number of lines of the requested file and to obtain the byte in which each line 
+starts so that we can tell each thread in which byte to start reading. we store that byte in the vector 
+"numLines" */
 int CountLines(std::string filename){
     int lines;
     std::string line;
@@ -64,7 +88,7 @@ int CountLines(std::string filename){
 
 	while(mFile.peek()!=EOF)
 	{
-        numLines.push_back(mFile.tellg()); //metemos en un vector el byte en el que comienza cada linea
+        numLines.push_back(mFile.tellg());
 		getline(mFile, line);
         lines++;
             
@@ -74,7 +98,7 @@ int CountLines(std::string filename){
 	return lines;
 
 }
-
+/* method to check that the arguments used are correct. Otherwise, the execution of the program ends. */
 void checkArguments(int argc, char **argv){
     if (argc!=4)
     {
@@ -87,10 +111,37 @@ void checkArguments(int argc, char **argv){
     }
     
 }
-
+//method used to check if the number of threads established is correct
 bool is_integer(char *str){
     while (*str)
         if (!isdigit(*str++))
             return false;
     return true;
+}
+
+/* method used to give each thread a turn to insert its search results into the global result vector */
+void assignTurns(int num_threads){
+
+    for (unsigned i = 0; i < num_threads; i++)
+    {
+        std::unique_lock<std::mutex> lk(m);
+        while (!flag){
+            flag=true;
+            lk.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            lk.lock();
+        }
+        
+    }
+}
+
+//method used to display the final results
+void printResults(){
+    for (int  i = 0; i < Totalsearches.size(); i++)
+    {
+        Result result = Totalsearches[i];
+        std::cout<< "[Hilo "<< BOLDYELLOW <<result.id << RESET<<" inicio: " << BOLDGREEN <<result.l_begin+1 
+        << RESET <<" - final: "<< BOLDGREEN << result.l_end+1 << RESET <<"] línea " << BOLDRED
+        << result.line<< RESET <<" :: ... "<< result.previous << " "<< BOLDBLUE<<result.word <<  RESET <<" "<< result.next << std::endl;
+    }
 }
--- include/Searcher.h
+++ include/Searcher.h
@@ -2,19 +2,29 @@
 #include <queue>
 #include <fstream>
 #include <bits/stdc++.h>
+#include <string>
+#include <mutex>
 #include "colors.h"
 
 #ifndef SEARCHER_H
 #define SEARCHER_H
 
 extern std::vector<int> numLines;
+extern std::mutex m;
+extern std::mutex vectorLock;
+extern int flag;
 
 struct Result{
     std::string previous;
     std::string next;
     std::string word;
+    int id;
+    int l_begin;
+    int l_end;
     int line;
 };
+
+extern std::vector<Result> Totalsearches;
 
 class Searcher{
     private:
@@ -23,18 +33,19 @@ class Searcher{
         int end;
         std::string filename; //archivo en el que se buscará la palabra
         std::string word; //palabra que se debe buscar
-        std::queue<Result> results;
+        std::vector<Result> results;
     public:
         Searcher(int id, int begin, int end, std::string filename, std::string word): id(id), 
         begin(begin), end(end),filename(filename),word(word){};
 
         void operator()(){
             searching();
-            printResults();
+            saveResults();
         }
         void searching();
         void findWord(std::string line, int numLine);
-        void printResults();
+        void saveResults();
+        bool checkWord(std::string checked);
 };
 
 void Searcher::searching(){
@@ -69,29 +80,81 @@ void Searcher::findWord(std::string line, int numLine){
         std::transform(tokens[i].begin(), tokens[i].end(), tokens[i].begin(), ::tolower);
         if (word==tokens[i] && i!=0 && i!=tokens.size()-1) //la palabra coincide y no es la primera ni la ultima de la linea
         {
-            std::cout<< tokens[i-1] << " - " << tokens[i] << " - " << tokens[i+1] << std::endl;
+            //std::cout<< tokens[i-1] << " - " << tokens[i] << " - " << tokens[i+1] << std::endl;
             Result coincidencia;
+            coincidencia.id=id;
             coincidencia.line=numLine;
             coincidencia.next=tokens[i+1];
             coincidencia.previous=tokens[i-1];
             coincidencia.word=tokens[i];
-            results.push(coincidencia);
+            coincidencia.l_begin=begin;
+            coincidencia.l_end=end;
+            results.push_back(coincidencia);
+        }
+        if (word==tokens[i] && i==0)
+        {
+            Result coincidencia;
+            coincidencia.id=id;
+            coincidencia.line=numLine;
+            coincidencia.next=tokens[i+1];
+            coincidencia.previous="";
+            coincidencia.word=tokens[i];
+            coincidencia.l_begin=begin;
+            coincidencia.l_end=end;
+            results.push_back(coincidencia);
+        }
+        if (word==tokens[i] && i==tokens.size()-1)
+        {
+            Result coincidencia;
+            coincidencia.id=id;
+            coincidencia.line=numLine;
+            coincidencia.next="";
+            coincidencia.previous=tokens[i-1];
+            coincidencia.word=tokens[i];
+            coincidencia.l_begin=begin;
+            coincidencia.l_end=end;
+            results.push_back(coincidencia);
         }
         
     }
 }
 
-void Searcher::printResults(){
-    
-    while (!results.empty())
+void Searcher::saveResults(){
+    /*
+    for (int  i = 0; i < results.size(); i++)
     {
-        Result resultado = results.back();
-        results.pop();
-        std::cout<< "[Hilo "<< id << " inicio: " << begin << " - final: "<< end << "] línea " << resultado.line<<
-        " :: ... "<< resultado.previous << " "<<resultado.word << " "<< resultado.next << std::endl; 
+        Result resultado = results[i];
+        std::cout<< "[Hilo "<< id << " inicio: " << begin+1 << " - final: "<< end+1 << "] línea " << resultado.line<<
+        " :: ... "<< resultado.previous << " "<<resultado.word << " "<< resultado.next << std::endl;
     }
+    */
+
+    std::unique_lock<std::mutex> lk(m);
+    while (!flag)
+    {
+        lk.unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        lk.lock();
+    }
+    std::unique_lock<std::mutex> vk(vectorLock);
+    for (unsigned i = 0; i < results.size(); i++)
+    {
+        Totalsearches.push_back(results[i]);
+    }
+    vk.unlock();
+    flag=false;
+   
     
-    
+}
+
+bool Searcher::checkWord(std::string checked){
+
+    const char *w = word.c_str();
+    const char *ck = checked.c_str();
+
+    if (strstr(w,ck)) return true;
+
+    return false;
 }
 
 #endif
