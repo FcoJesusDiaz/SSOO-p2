6b9b48069c4cce745f8111ac64d8988f20d92282
JavierVillarAsensio <72606232+JavierVillarAsensio@users.noreply.github.com>
2022-05-02 20:30:27 +0200

    Add files via upload

---
+++ include/Request.h
@@ -0,0 +1,23 @@
+#ifndef REQUEST
+#define REQUEST
+
+class Request{
+    private:
+        int& balance;
+        std::future<std::string> fut;
+        std::promise<std::string> prom;
+        int client_id;
+        int type;
+        std::string word;
+    public:
+        Request(int& balance, int client_id, int type, std::string word): 
+            balance(balance), client_id(client_id), type(type), word(word){};
+        void set_promise_value(int value);
+        std::string get_future_value();
+        int getClientID();
+        int getType();
+        int decrease_balance();
+        std::string getWord();
+};
+
+#endif
