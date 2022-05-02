c44cfce0051121b11d96da6778215fc77e5ee8f9
JavierVillarAsensio <72606232+JavierVillarAsensio@users.noreply.github.com>
2022-05-02 20:29:03 +0200

    Update client.cpp
    añadido método que hace una búsqueda

--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,14 @@
 #include "client.h"
 
-void Client::operator()(){
-    
+#include <string>
+
+std::string Client::make_search(std::string word){
+    Request req{balance, id, type, word};
+    cola_de_requests.push(std::ref(req));
+    variable_de_aceso_a_cola_de_request.notify_one();
+    return req.get_future_value();
+}
+
+void Clien::setBalance(int new_balance){
+    balance = new_balance;
 }
